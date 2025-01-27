676d34fbd48df5e0f604c3843c87babbb7b45775
UnicornParson <41113975+UnicornParson@users.noreply.github.com>
2025-01-27 17:19:20 +0300

    playgroung initial

--- playground/gl_example.py
+++ playground/gl_example.py
@@ -3,6 +3,8 @@ from pygame.locals import *
 
 from OpenGL.GL import *
 from OpenGL.GLU import *
+import cv2
+import numpy as np
 
 def draw_cube():
     glBegin(GL_QUADS)
@@ -59,17 +61,38 @@ def main():
     gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
 
     glTranslatef(0.0,0.0, -5)
-
-    while True:
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter('output.avi', fourcc, 30.0, display)
+    framenum = 0
+    while framenum < 500:
+        framenum += 1
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-                quit()
+                break
 
         glRotatef(1, 3, 1, 1)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
         draw_cube()
         pygame.display.flip()
-        pygame.time.wait(10)
+        #pygame.time.wait(10)
 
+        #buffer = pygame.image.tostring(pygame.display.get_surface(), 'RGB', True)
+
+        #surface_buffer = pygame.image.tostring(screen, 'RGB')
+        surface_buffer = pygame.image.tostring(pygame.display.get_surface(), 'RGB', True)
+        #surface_np = np.frombuffer(surface_buffer, dtype=np.uint8).reshape((display[1], display[0], 3))
+        surface_np = np.frombuffer(surface_buffer, dtype=np.uint8).reshape((display[1], display[0], 3))
+        print(surface_np.shape)
+        cv2_image = cv2.cvtColor(surface_np, cv2.COLOR_RGB2BGR)
+
+        #decoded = cv2.imdecode(np.fromstring(buffer, np.uint8), cv2.IMREAD_COLOR)
+        #img = decoded.astype(np.uint8)[:, :, ::-1]
+        #out.write(cv2_image)
+
+        cv2.imwrite(f"./i/{framenum}.png", cv2_image)
+    out.release()
+    cv2.destroyAllWindows()
 main()
+
+
